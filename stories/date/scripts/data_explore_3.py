ae1ecd9fec63515194955dcc59f97787ed48c93f
tylerenglish <eng20007@byui.edu>
2023-06-20 13:40:08 -0600

    made the date split

---
+++ stories/date/scripts/data_explore_3.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+# df = pd.read_csv('stories/date/data/text_date_journals.csv')
+df = pd.read_csv('stories/date/data/text_date_full.csv')
+
+print(df.head())
+
+for i in range(10):
+    print(df['date'][i])
+    print('-----------------------------')
+    print(df['text'][i])
+    print('\n\n')
+
+
+
+
+for i in range(len(df['date'])-10, len(df['date'])):
+    print(df['date'][i])
+    print('-----------------------------')
+    print(df['text'][i])
+    print('\n\n')
+
+
+
+
+
+
+# URL = 'https://raw.githubusercontent.com/wilfordwoodruff/Main-Data/main/data/derived/derived_data.csv'
+
+# df = pd.read_csv(URL)
+
+# print(df['Document Type'].unique())
